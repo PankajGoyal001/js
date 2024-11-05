50b026291426e0fde490b496a5487efa997f2831
PankajGoyal001 <goyalpankaj85324@gmail.com>
2024-11-05 13:36:08 +0530

    new

---
+++ index.js
@@ -0,0 +1,45 @@
+
+let value05 = 15 + 15;
+let value1 = 15 + 15;
+
+console.log(value + value1);
+
+{
+  let value0 = 41321;
+  let value01 = 566;
+  console.log(value0 - value01);
+}
+
+var value = 15 + 15;
+
+
+{
+  var value2 = 64516;
+  var value3 = 2121;
+  console.log(value2 - value3);
+}
+
+const value4 = 15 + 15;
+const value5 = 15 + 15;
+
+console.log(value4 + value5);
+
+{
+  const value04 = 465454;
+  const value05 = 55654;
+
+  console.log(value04 - value05);
+}
+
+
+let a = 50;
+let b = 6543;
+
+console.log(b)
+console.log(a)
+
+ var x = "dmfvghdf"
+ console.log(x ) 
+
+
+
